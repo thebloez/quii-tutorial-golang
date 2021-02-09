f3c54b047e531ca4223234f44754a1f8cbdd2edc
Thebloez <ryandunksfamz@gmail.com>
2021-02-09 14:56:46 +0700

    pointer section (to be continued)

---
+++ pointer_and_errors/wallet.go
@@ -0,0 +1,30 @@
+package pointer_and_errors
+
+import "fmt"
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	w.balance -= amount
+	return nil
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
