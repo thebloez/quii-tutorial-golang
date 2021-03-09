a61d5edafbd487acef62fe4c9e48398119c5d29b
thebloez <ryandunksfamz@gmail.com>
2021-03-09 14:58:37 +0700

    dependency injection section

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	_ "quii-tutorial/dependency-injection"
+)
+
+func main() {
+	Greet(os.Stdout, "Elodie")
+	http.ListenAndServe(":5000", http.HandlerFunc(myGreeterHandler))
+}
+
+func myGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
